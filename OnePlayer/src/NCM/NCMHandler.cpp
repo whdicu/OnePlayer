339dcf8a5286b2a9e62c5ec1b52708b1f9c0539a
OneHidder <1467058574@qq.com>
2023-12-10 14:04:46 +0800

    1.增加NCM相关文件

---
+++ OnePlayer/src/NCM/NCMHandler.cpp
@@ -0,0 +1,104 @@
+﻿#include "NCMHandler.h"
+#include "ImageHandler.h"
+#include <QDebug>
+#include <QEventLoop>
+#include <QFile>
+#include <QImage>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+extern "C"
+{
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/imgutils.h>
+}
+
+
+CPPMusicData NCMHandler::dealNCM(const QString& filename)
+{
+	CPPMusicData ret;
+	std::string fileStr = filename.toLocal8Bit().toStdString();
+	//std::string outputDirStr = outputDir.toLocal8Bit().toStdString();
+	CMusicData musicData;
+	int status = getFileData(fileStr.c_str(), &musicData);
+	ret.data = QByteArray(reinterpret_cast<const char*>(musicData.data), musicData.dataSize);
+
+	QJsonParseError parseError;
+	QJsonDocument jsonDoc = QJsonDocument::fromJson(
+		QString::fromLocal8Bit(musicData.jsonStr).toUtf8(), &parseError);
+	if (QJsonParseError::NoError != parseError.error)
+	{
+		qWarning() << __FUNCTION__ << "JsonStr analyze failed";
+		return ret;
+	}
+
+	QJsonObject obj = jsonDoc.object();
+	/*
+	
+	{
+		"musicId":	"track-108914",
+		"musicName":	"江南",
+		"artist":	[["林俊杰", "3684"]],
+		"albumId":	"10804",
+		"album":	"第二天堂",
+		"albumPicDocId":	"109951168111265583",
+		"albumPic":	"http://p3.music.126.net/_0OAhWhIbg-nOP-6e4o-SA==/109951168111265583.jpg",
+		"bitrate":	128000,
+		"mp3DocId":	"811c35a7f7f552989edcaea92871b544",
+		"duration":	267946,
+		"mvId":	"522362",
+		"alias":	[],
+		"transNames":	["River South"],
+		"format":	"mp3",
+		"fee":	1,
+		"privilege":	{
+			"flag":	1278212
+		}
+	}
+	
+	*/
+
+	ret.title = obj["musicName"].toString();
+	QJsonArray artists = obj["artist"].toArray().first().toArray();
+	QString artistsStr;
+	for (auto artist : artists)
+		artistsStr.append(artist.toString()).append(' ');
+	ret.singers = artistsStr.trimmed();
+	ret.album = obj["album"].toString();
+
+	if (obj.contains("albumPic"))
+	{
+		QString url = obj["albumPic"].toString();
+		ret.image = ImageHandler::downloadImage(url);
+	}
+
+	//if (!albumImg.isNull())
+	//{
+ //       //addImage2Data(musicData, albumImg, musicData);
+ //       //func1(musicData);
+ //       //addAlbumCover(albumImg);
+
+	//	//qint64 i1 = QDateTime::currentMSecsSinceEpoch();
+	//	//albumImg.save("C:\\Users\\WHDon\\Desktop\\asdas.jpg");
+	//	//qDebug() << (QDateTime::currentMSecsSinceEpoch() - i1);
+	//}
+
+	//QFile file(outputDir + "/asd.mp3");
+	//file.open(QIODevice::WriteOnly);
+	//file.write(musicData);
+	//file.close();
+
+
+	//QVariantMap map = obj.toVariantMap();
+	//for (auto it = map.begin(); it != map.end(); ++it)
+	//	qDebug() << it.key() << it.value();
+
+	free(musicData.data);
+	free(musicData.jsonStr);
+
+	return ret;
+}
---
+++ OnePlayer/src/NCM/NCMHandler.h
@@ -0,0 +1,23 @@
+/*
+ncmToMp3.h 对 QT的接口
+*/
+#pragma once
+#include <QImage>
+extern "C" {
+	#include "ncmToMp3.h"
+}
+
+struct CPPMusicData
+{
+	QByteArray data;
+	QString title;
+	QString singers;
+	QString album;
+	QImage image;
+};
+
+namespace NCMHandler
+{
+	CPPMusicData dealNCM(const QString& filename);
+};
+
