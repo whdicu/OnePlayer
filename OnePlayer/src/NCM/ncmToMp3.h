7b1406fbee7f845d761eaee942b707de53ebd87b
wang.haodong <wang.haodong@hzleaper.com>
2023-12-11 11:40:23 +0800

    1.修改结构体代码位置

--- OnePlayer/src/NCM/ncmToMp3.h
+++ OnePlayer/src/NCM/ncmToMp3.h
@@ -1,16 +1,16 @@
 ﻿#ifdef WIN32
 #include<Windows.h>
 
+//返回转换好的字符串指针
+unsigned char* utf8ToGbk(unsigned char* src, int len);
+#endif
+
 struct CMusicData
 {
 	unsigned char* data;
 	int dataSize;
 	char* jsonStr;
 };
-
-//返回转换好的字符串指针
-unsigned char* utf8ToGbk(unsigned char* src, int len);
-#endif
 
 void swap(unsigned char* a, unsigned char* b);
 
